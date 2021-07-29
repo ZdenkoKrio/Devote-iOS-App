8016a96d565d6d9c15812cab8862fd5a080572e3
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2021-07-30 00:20:32 +0200

    cleaning project files

---
+++ Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Zdenko Čepan on 30/07/2021.
+//
+
+import Foundation
+
+// MARK: - Formatter
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
